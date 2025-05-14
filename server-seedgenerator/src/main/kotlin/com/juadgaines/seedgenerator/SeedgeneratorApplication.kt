76236da78865e8790c320806fdb086717ddfb81e
Juan Gaines <juandavidgaines@gmail.com>
2025-05-14 14:22:55 -0500

    feat: springboot server code from Spring initializer

---
+++ server-seedgenerator/src/main/kotlin/com/juadgaines/seedgenerator/SeedgeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.juadgaines.seedgenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeedgeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeedgeneratorApplication>(*args)
+}
