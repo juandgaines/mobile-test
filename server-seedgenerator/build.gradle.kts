a878f750d754752dba7c0e5e15f1cbd917e4ca83
Juan Gaines <juandavidgaines@gmail.com>
2025-05-14 15:23:53 -0500

    feat: profile file to deploy to heroku

--- server-seedgenerator/build.gradle.kts
+++ server-seedgenerator/build.gradle.kts
@@ -3,11 +3,14 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
+	application
 }
 
 group = "com.juadgaines"
 version = "0.0.1-SNAPSHOT"
-
+application {
+	mainClass.set("com.juadgaines.seedgenerator.SeedGeneratorApplicationKt")
+}
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(17)
