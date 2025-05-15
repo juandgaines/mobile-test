da59f41a5203e9955b8bf3e1e46e8d01a06ad23c
Juan Gaines <juandavidgaines@gmail.com>
2025-05-15 12:00:36 -0500

    feat: camera x dependencies

--- androidapp/app/build.gradle.kts
+++ androidapp/app/build.gradle.kts
@@ -85,6 +85,14 @@ dependencies {
     implementation(libs.room.runtime)
     ksp(libs.room.compiler)
 
+    //camera
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.video)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.extensions)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- androidapp/app/src/main/java/com/juandgaines/seedqrvalidator/scanner/presentation/ScannerScreen.kt
+++ androidapp/app/src/main/java/com/juandgaines/seedqrvalidator/scanner/presentation/ScannerScreen.kt
@@ -1,8 +1,26 @@
 package com.juandgaines.seedqrvalidator.scanner.presentation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.common.Barcode
 
 @Composable
 fun ScannerScreenRoot() {
     // Scanner screen implementation
+
+    Column {
+        Button(onClick = {
+            val options = BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(
+                    Barcode.FORMAT_QR_CODE
+                )
+                .build()
+        }) {
+            Text("Scan")
+        }
+    }
 }
+
