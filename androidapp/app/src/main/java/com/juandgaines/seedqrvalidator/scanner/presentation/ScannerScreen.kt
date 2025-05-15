cdeb1d5c9c42e5bdb55586bbc51a4b9c9a0b5585
Juan Gaines <juandavidgaines@gmail.com>
2025-05-15 14:21:57 -0500

    feat: camera permission logic

--- androidapp/app/src/main/java/com/juandgaines/seedqrvalidator/scanner/presentation/ScannerScreen.kt
+++ androidapp/app/src/main/java/com/juandgaines/seedqrvalidator/scanner/presentation/ScannerScreen.kt
@@ -1,26 +1,79 @@
 package com.juandgaines.seedqrvalidator.scanner.presentation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import android.Manifest
+import android.content.Context
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
-import com.google.mlkit.vision.barcode.BarcodeScannerOptions
-import com.google.mlkit.vision.barcode.common.Barcode
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.juandgaines.seedqrvalidator.core.presentation.navigation.Destinations
+import com.juandgaines.seedqrvalidator.core.presentation.utils.hasCameraPermission
+import com.juandgaines.seedqrvalidator.core.presentation.utils.shouldShowCameraPermissionRationale
 
 @Composable
-fun ScannerScreenRoot() {
+fun ScannerScreenRoot(
+    scannerScannerViewModel: ScannerViewModel,
+    navigateToDestination: (Destinations) -> Unit,
+    navigateUp: () -> Unit,
+) {
     // Scanner screen implementation
 
-    Column {
-        Button(onClick = {
-            val options = BarcodeScannerOptions.Builder()
-                .setBarcodeFormats(
-                    Barcode.FORMAT_QR_CODE
-                )
-                .build()
-        }) {
-            Text("Scan")
+    val scannerState by scannerScannerViewModel.scannerState.collectAsStateWithLifecycle()
+
+    ScannerScreen(
+        state = scannerState,
+        onEvent = scannerScannerViewModel::onAction
+    )
+}
+
+@Composable
+fun ScannerScreen(
+    state:ScannerState,
+    onEvent: (ScannerIntent) -> Unit,
+) {
+    val context = LocalContext.current
+    val activity = context as ComponentActivity
+    val permissionLauncherCamera = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    )
+    { _->
+
+        val cameraRationale = activity.shouldShowCameraPermissionRationale()
+        onEvent(
+            ScannerIntent.SubmitCameraPermissionInfo(
+                acceptedCameraPermission = activity.hasCameraPermission(),
+                showCameraRationale = cameraRationale
+            )
+        )
+    }
+
+    LaunchedEffect(key1 = true) {
+        val showCameraRationale = activity.shouldShowCameraPermissionRationale()
+
+        onEvent(
+            ScannerIntent.SubmitCameraPermissionInfo(
+                acceptedCameraPermission = activity.shouldShowCameraPermissionRationale(),
+                showCameraRationale = showCameraRationale
+            )
+        )
+
+        if (!showCameraRationale) {
+            permissionLauncherCamera.requestCameraScreenPermissions(context)
         }
+    }
+}
+
+private fun ManagedActivityResultLauncher<String, Boolean>.requestCameraScreenPermissions(
+    context: Context
+) {
+    val hasCameraPermission = context.hasCameraPermission()
+    if (!hasCameraPermission) {
+        launch(Manifest.permission.CAMERA)
     }
 }
 
--- androidapp/app/src/main/java/com/juandgaines/seedqrvalidator/scanner/presentation/ScannerViewModel.kt
+++ androidapp/app/src/main/java/com/juandgaines/seedqrvalidator/scanner/presentation/ScannerViewModel.kt
@@ -3,8 +3,10 @@ package com.juandgaines.seedqrvalidator.scanner.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -13,6 +15,10 @@ import javax.inject.Inject
 class ScannerViewModel @Inject constructor(
 
 ): ViewModel(){
+
+
+    private val _eventChannel = Channel<ScannerEvents>()
+    val eventsHome = _eventChannel.receiveAsFlow()
 
     private val _currentPreviewPhoto = MutableStateFlow<ByteArray?>(null)
 
@@ -37,7 +43,7 @@ class ScannerViewModel @Inject constructor(
     }
 
 
-    suspend fun onCancelPicturePreview() {
+    private fun onCancelPicturePreview() {
         _currentPreviewPhoto.value = null
     }
 
