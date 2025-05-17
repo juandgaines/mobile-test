2e20fe963f25c4199d13b5ad2d4001545b931112
Juan Gaines <juandavidgaines@gmail.com>
2025-05-17 08:00:15 -0500

    fix: better approach

--- androidapp/app/src/main/java/com/juandgaines/seedqrvalidator/scanner/presentation/CameraPreview.kt
+++ androidapp/app/src/main/java/com/juandgaines/seedqrvalidator/scanner/presentation/CameraPreview.kt
@@ -41,12 +41,6 @@ fun CameraPreview(
                 val options = BarcodeScannerOptions.Builder()
                     .setBarcodeFormats(
                         Barcode.FORMAT_QR_CODE,
-                        Barcode.FORMAT_CODABAR,
-                        Barcode.FORMAT_CODE_93,
-                        Barcode.FORMAT_CODE_39,
-                        Barcode.FORMAT_CODE_128,
-                        Barcode.FORMAT_EAN_8,
-                        Barcode.FORMAT_EAN_13,
                         Barcode.FORMAT_AZTEC
                     )
                     .build()
